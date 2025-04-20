3f236c54c7147e543e5bc0057cebdbb56c7ce165
Joydip <joydip.gope02@gmail.com>
2025-04-20 20:02:30 +0600

    login and signup pages complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import CaptainLogin from './pages/CaptainLogin'
+import CaptainSignup from './pages/CaptainSignup'
 
 const App = () => {
   return (
     <div>
-      APP
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/login' element={<UserLogin />}></Route>
+        <Route path='/signup' element={<UserSignup />}></Route>
+        <Route path='/captain-login' element={<CaptainLogin />}></Route>
+        <Route path='/captain-signup' element={<CaptainSignup />}></Route>
+      </Routes>
     </div>
   )
 }
